6ba9f213d390c336a02ac1492d4b27f91165089e
Tony <Tony@test.com>
2019-06-27 09:31:25 +0800

    增加协变，逆变例子

--- src/test/java/com/github/yanglikun/generic/VariantTest.java
+++ src/test/java/com/github/yanglikun/generic/VariantTest.java
@@ -1,7 +1,10 @@
 package com.github.yanglikun.generic;
 
+import generic.variant.SubType;
+import generic.variant.SuperType;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,4 +24,44 @@ public class VariantTest {
     }
 
 
+    /**
+     * 数组是协变的
+     */
+    @Test
+    public void testArray() {
+        SuperType[] superArr = new SuperType[3];
+        SubType[] subArr = new SubType[3];
+        superArr = subArr;
+    }
+
+    /**
+     * 泛型是不变的
+     */
+    @Test
+    public void testGeneric() {
+        List<Object> objectList = null;
+        List<String> stringList = null;
+        //objectList = stringList;//编译报错
+    }
+
+    /**
+     * extend是协变的
+     */
+    @Test
+    public void testGenericExtends() {
+        List<? extends SuperType> superExtendList = new ArrayList<>();
+        List<SubType> subList = new ArrayList<>();
+
+        superExtendList = subList;
+    }
+
+    /**
+     * super是逆变的
+     */
+    @Test
+    public void testGenericSuper() {
+        List<? super SubType> subSuperList = new ArrayList<>();
+        List<SuperType> superList = new ArrayList<>();
+        subSuperList = superList;
+    }
 }
